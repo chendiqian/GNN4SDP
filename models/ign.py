f2228ed60078e08a4ad8155909e9f38f584b0ed0
Chendi Qian <chendiqian0111@gmail.com>
2025-06-23 12:48:42 +0800

    reduce basis of ign

--- models/ign.py
+++ models/ign.py
@@ -17,7 +17,7 @@ class Layer2to1(nn.Module):
         self.normalization = normalization
         self.normalization_val = normalization_val
 
-        self.basis_dimension = 5
+        self.basis_dimension = 2
 
         # initialization values for variables
         self.coeffs = nn.Parameter(
@@ -60,7 +60,7 @@ class Layer1to2(nn.Module):
         self.normalization = normalization
         self.normalization_val = normalization_val
 
-        self.basis_dimension = 5
+        self.basis_dimension = 2
 
         # initialization values for variables
         self.coeffs = nn.Parameter(
@@ -92,62 +92,62 @@ class Layer1to2(nn.Module):
 def contractions_2_to_1(inputs, dim, normalization='inf', normalization_val=1.0):  # N x D x m x m
     diag_part = torch.diagonal(inputs, dim1=2, dim2=3)  # N x D x m
 
-    sum_diag_part = torch.sum(diag_part, dim=2, keepdim=True)  # N x D x 1
+    # sum_diag_part = torch.sum(diag_part, dim=2, keepdim=True)  # N x D x 1
     sum_of_rows = torch.sum(inputs, dim=3)  # N x D x m
-    sum_of_cols = torch.sum(inputs, dim=2)  # N x D x m
-    sum_all = torch.sum(inputs, dim=(2, 3))  # N x D
+    # sum_of_cols = torch.sum(inputs, dim=2)  # N x D x m
+    # sum_all = torch.sum(inputs, dim=(2, 3))  # N x D
 
     # op1 - (123) - extract diag
     op1 = diag_part  # N x D x m
 
     # op2 - (123) + (12)(3) - tile sum of diag part
-    op2 = sum_diag_part.repeat(1, 1, dim)
+    # op2 = sum_diag_part.repeat(1, 1, dim)
     # op2 = torch.cat([sum_diag_part for d in range(dim)], dim=2)  # N x D x m
 
     # op3 - (123) + (13)(2) - place sum of row i in element i
     op3 = sum_of_rows  # N x D x m
 
     # op4 - (123) + (23)(1) - place sum of col i in element i
-    op4 = sum_of_cols  # N x D x m
+    # op4 = sum_of_cols  # N x D x m
 
     # op5 - (1)(2)(3) + (123) + (12)(3) + (13)(2) + (23)(1) - tile sum of all entries
-    op5 = sum_all.unsqueeze(dim=2).repeat(1, 1, dim)
+    # op5 = sum_all.unsqueeze(dim=2).repeat(1, 1, dim)
     # op5 = torch.cat([sum_all.unsqueeze(dim=2) for d in range(dim)], dim=2)  # N x D x m
 
     if normalization is not None:
         if normalization == 'inf':
-            op2 = op2 / dim
+            # op2 = op2 / dim
             op3 = op3 / dim
-            op4 = op4 / dim
-            op5 = op5 / (dim ** 2)
+            # op4 = op4 / dim
+            # op5 = op5 / (dim ** 2)
 
-    return [op1, op2, op3, op4, op5]
+    return [op1, op3,]
 
 
 # ops_1_to_2
 def contractions_1_to_2(inputs, dim, normalization='inf', normalization_val=1.0):  # N x D x m x m
-    sum_all = torch.sum(inputs, dim=2, keepdim=True)  # N x D x 1
+    # sum_all = torch.sum(inputs, dim=2, keepdim=True)  # N x D x 1
 
     # op1 - (123) - place on diag
     op1 = torch.diag_embed(inputs, dim1=2, dim2=3)  # N x D x m x m
 
     # op2 - (123) + (12)(3) - tile sum on diag
-    op2 = torch.diag_embed(sum_all.repeat(1, 1, dim), dim1=2, dim2=3)  # N x D x m x m
+    # op2 = torch.diag_embed(sum_all.repeat(1, 1, dim), dim1=2, dim2=3)  # N x D x m x m
 
     # op3 - (123) + (13)(2) - tile element i in row i
-    op3 = inputs.unsqueeze(2).repeat(1, 1, dim, 1)
+    # op3 = inputs.unsqueeze(2).repeat(1, 1, dim, 1)
     # op3 = torch.cat([torch.unsqueeze(inputs, dim=2) for d in range(dim)], dim=2)  # N x D x m x m
 
     # op4 - (123) + (23)(1) - tile element i in col i
-    op4 = inputs.unsqueeze(3).repeat(1, 1, 1, dim)
+    # op4 = inputs.unsqueeze(3).repeat(1, 1, 1, dim)
     # op4 = torch.cat([torch.unsqueeze(inputs, dim=3) for d in range(dim)], dim=3)  # N x D x m x m
 
     # op5 - (1)(2)(3) + (123) + (12)(3) + (13)(2) + (23)(1) - tile sum of all entries
-    op5 = sum_all.unsqueeze(3).repeat(1, 1, dim, dim)  # N x D x m x m
+    # op5 = sum_all.unsqueeze(3).repeat(1, 1, dim, dim)  # N x D x m x m
 
-    if normalization is not None:
-        if normalization == 'inf':
-            op2 = op2 / dim
-            op5 = op5 / dim
+    # if normalization is not None:
+    #     if normalization == 'inf':
+            # op2 = op2 / dim
+            # op5 = op5 / dim
 
-    return [op1, op2, op3, op4, op5]
+    return [op1, inputs.unsqueeze(2) + inputs.unsqueeze(3)]
