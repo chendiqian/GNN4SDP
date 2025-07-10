c6bf4b8b6464d883ef74532a62298c5310404571
Chendi Qian <chendiqian0111@gmail.com>
2025-07-10 19:32:34 +0800

    clean unused

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -8,78 +8,6 @@ from torch_geometric.utils import scatter
 from torch_sparse import SparseTensor, spmm
 
 
-def gurobi_solve_lp(A, b, c, lb=0., ub=float('inf')):
-    import gurobipy as gp
-    from gurobipy import GRB
-
-    m, n = A.shape
-    model = gp.Model("lp")
-    model.Params.LogToConsole = 0
-    variables = model.addMVar(n, lb=lb, ub=ub)
-
-    # Objective: 0.5 x^T P x + q^T x
-    model.setObjective(c @ variables, GRB.MINIMIZE)
-
-    # Add inequality constraints
-    constrs = model.addConstr(A @ variables <= b)
-
-    # Solve
-    model.optimize()
-
-    # Duals
-    if model.status == GRB.OPTIMAL:
-        duals = constrs.getAttr("Pi")
-        solution = variables.X
-    else:
-        duals = solution = None
-    return solution, duals
-
-
-def gurobi_solve_qp(Q, c, Aub, bub, Aeq=None, beq=None, lb=0., ub=float('inf')):
-    import gurobipy as gp
-    from gurobipy import GRB
-
-    _, n = Aub.shape
-    model = gp.Model("qp")
-    model.Params.LogToConsole = 0
-    variables = model.addMVar(n, lb=lb, ub=ub)
-
-    # Objective: 0.5 x^T P x + q^T x
-    model.setObjective(0.5 * variables @ Q @ variables + c @ variables)
-
-    # Add inequality constraints
-    constrs = model.addConstr(Aub @ variables <= bub)
-    if Aeq is not None:
-        constrs2 = model.addConstr(Aeq @ variables == beq)
-
-    # Solve
-    model.optimize()
-
-    # Duals
-    if model.status == GRB.OPTIMAL:
-        duals = constrs.getAttr("Pi")
-        if Aeq is not None:
-            duals = np.hstack([duals, constrs2.getAttr("Pi")])
-        solution = variables.X
-    else:
-        duals = solution = None
-    return solution, duals
-
-
-# def recover_lp_from_data(data, dtype=np.float32):
-#     data = data.to('cpu')
-#     c = data.q.numpy().astype(dtype)
-#     b = data.b.numpy().astype(dtype)
-#     A = SparseTensor(row=data['cons', 'to', 'vals'].edge_index[0],
-#                      col=data['cons', 'to', 'vals'].edge_index[1],
-#                      value=data['cons', 'to', 'vals'].edge_attr.squeeze(),
-#                      sparse_sizes=(data['cons'].num_nodes, data['vals'].num_nodes)).to_scipy('csr').toarray().astype(dtype)
-#     # todo: might vary
-#     lb = np.zeros(A.shape[1]).astype(dtype)
-#     ub = None
-#     return A, c, b, lb, ub
-#
-#
 # def recover_qp_from_data(data, dtype=np.float32):
 #     data = data.to('cpu')
 #     c = data.q.numpy().astype(dtype)
@@ -92,7 +20,6 @@ def gurobi_solve_qp(Q, c, Aub, bub, Aeq=None, beq=None, lb=0., ub=float('inf')):
 #                      col=data['vals', 'to', 'vals'].edge_index[1],
 #                      value=data['vals', 'to', 'vals'].edge_attr.squeeze(),
 #                      sparse_sizes=(data['vals'].num_nodes, data['vals'].num_nodes)).to_scipy('csr').toarray().astype(dtype)
-#     # todo: might vary
 #     lb = np.zeros(A.shape[1]).astype(dtype)
 #     ub = None
 #     return P, A, c, b, lb, ub
@@ -108,16 +35,3 @@ def normalize_cons(A, b):
     A = Ab[:, :-1]
     b = Ab[:, -1]
     return A, b
-
-
-# def calc_violation(pred, data):
-#     assert pred.dim() <= 2
-#     if pred.dim() == 2:
-#         assert pred.shape[1] == 1
-#     if pred.dim() == 1:
-#         pred = pred[:, None]
-#     Ax_minus_b = spmm(data['cons', 'to', 'vals'].edge_index,
-#                       data['cons', 'to', 'vals'].edge_attr.squeeze(),
-#                       data['cons'].num_nodes, data['vals'].num_nodes, pred).squeeze() - data.b
-#     violation = scatter(torch.relu(Ax_minus_b), data['cons'].batch, dim=0, reduce='mean')  # (batchsize,)
-#     return violation
