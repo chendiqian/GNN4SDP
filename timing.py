0994c905c555fd0fd3e3dabb8d8abb7ad4981988
Chendi Qian <chendiqian0111@gmail.com>
2025-09-01 11:03:44 +0200

    fix timing

--- timing.py
+++ timing.py
@@ -5,18 +5,18 @@ import wandb
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import warnings
 
 from data.collate_func import collate_fn_lp_base
 from data.dataset import LPDataset
 from models import get_model
-from utils.experiment import save_run_config, setup_wandb, sync_timer
+from utils.experiment import setup_wandb, sync_timer
 
 torch.set_float32_matmul_precision('high')
 
 
-@hydra.main(version_base=None, config_path='./config', config_name="mpnn")
+@hydra.main(version_base=None, config_path='./config', config_name="ppgn_time")
 def main(args: DictConfig):
-    log_folder_name = save_run_config(args)
     setup_wandb(args)
 
     test_set = LPDataset(args.train.datapath, 'test', transform=None)
@@ -30,7 +30,13 @@ def main(args: DictConfig):
                              collate_fn=collate_fn_lp_base,
                              pin_memory=True)
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = args.train.device
+    assert device in ['cuda', 'cpu']
+    if device == 'cuda' and torch.cuda.is_available():
+        device = 'cuda'
+    else:
+        warnings.warn("No cuda available")
+        device = 'cpu'
 
     model = get_model(args.gnn).to(device)
     with torch.no_grad():
@@ -53,6 +59,7 @@ def main(args: DictConfig):
     wandb.log({
         'time_mean': np.mean(times),
         'time_std': np.std(times),
+        'time_string': f'{np.mean(times):.3f}±{np.std(times):.3f}'
     })
 
 
